f8429c439dda4c942ab88658783d6c84ad50f497
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-02-13 21:58:22 +0300

    Business and DataAccess layers objects added for categories and product entities. Controllers are added.

---
+++ BookStore.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using BookStore.BusinessLayer.Abstract;
+using BookStore.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult ProductList()
+        {
+            var value = _productService.TGetAll();
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            _productService.TAdd(product);
+            return Ok("Ekleme işlemi başarılı");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProduct(Product product)
+        {
+            _productService.TUpdate(product);
+            return Ok("Güncelleme işlemi başarılı");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProduct(int id)
+        {
+            _productService.TDelete(id);
+            return Ok("Silme işlemi başarılı");
+        }
+
+        [HttpGet("GetProduct")]
+        public IActionResult GetProduct(int id)
+        {
+            var value = _productService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpGet("ProductCount")]
+        public IActionResult ProductCount()
+        {
+            var value = _productService.TGetProductCount();
+            return Ok(value);
+        }
+    }
+}
