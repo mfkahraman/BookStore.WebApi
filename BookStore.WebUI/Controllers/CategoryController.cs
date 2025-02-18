cdbb65dbc68bb97998006aac840e7f3b5a982e00
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-02-18 20:30:01 +0300

    UI layer added

---
+++ BookStore.WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.WebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public async Task<IActionResult> CategoryList()
+        {
+            return View();
+        }
+    }
+}
